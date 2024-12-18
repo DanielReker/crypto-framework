fbd9e10d17fc8eeadf8173eefcf066c3f2e36471
DanielReker <sasha29072004@gmail.com>
2024-12-17 19:01:28 -0800

    Fix MscapiCertificate names

--- src/MscapiCertificate.cpp
+++ src/MscapiCertificate.cpp
@@ -2,20 +2,20 @@
 #include "MscapiCsp.hpp"
 
 
-MscapiCertificate::MscapiCertificate(const MscapiCsp& vip_net_csp, const std::string& subject_name, _MscapiCertificate* context) :
-	vip_net_csp_(vip_net_csp), subject_name_(subject_name), context_(context) {
+MscapiCertificate::MscapiCertificate(const MscapiCsp& mscapi_csp, const std::string& subject_name, _MscapiCertificate* context) :
+	mscapi_csp_(mscapi_csp), subject_name_(subject_name), context_(context) {
 }
 
 Blob MscapiCertificate::Encrypt(const Blob& data) const {
-	return vip_net_csp_.EncryptWithCertificate(data, *this);
+	return mscapi_csp_.EncryptWithCertificate(data, *this);
 }
 
 Blob MscapiCertificate::Decrypt(const Blob& encrypted_data) const {
-	return vip_net_csp_.DecryptWithCertificate(encrypted_data, *this);
+	return mscapi_csp_.DecryptWithCertificate(encrypted_data, *this);
 }
 
 Blob MscapiCertificate::SignCades(const Blob& data, CadesType type, bool detached, const std::wstring& tsp_server_url) const {
-	return vip_net_csp_.SignCadesWithCertificate(data, type, *this, detached, tsp_server_url);
+	return mscapi_csp_.SignCadesWithCertificate(data, type, *this, detached, tsp_server_url);
 }
 
 std::string MscapiCertificate::GetSubjectName() const {
--- src/include/MscapiCertificate.hpp
+++ src/include/MscapiCertificate.hpp
@@ -11,12 +11,12 @@ class MscapiCsp;
 
 class MscapiCertificate : public ICertificate {
 private:
-    const MscapiCsp& vip_net_csp_;
+    const MscapiCsp& mscapi_csp_;
     std::string subject_name_;
     _MscapiCertificate* context_;
 
 public:
-    MscapiCertificate(const MscapiCsp& vip_net_csp, const std::string& subject_name, _MscapiCertificate* context);
+    MscapiCertificate(const MscapiCsp& mscapi_csp, const std::string& subject_name, _MscapiCertificate* context);
     _MscapiCertificate* GetCertContext() const;
 
     Blob Encrypt(const Blob& data) const override;
